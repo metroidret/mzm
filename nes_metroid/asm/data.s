0292b8e3d7b2835a7224b12292f67b2127bbafd8
CharlesVanEeckhout <vaneeckhoutcharles@gmail.com>
2026-04-11 17:12:54 -0400

    makefile compress

--- nes_metroid/asm/data.s
+++ nes_metroid/asm/data.s
@@ -1,4 +1,6 @@
     .include "asm/constants.inc"
     .include "asm/macros.inc"
 
-    @ .incbin "payload/payload.gba.lz"
+    .incbin "payload/payload_us.gba.lz"
+    .align 4, 0
+    .incbin "emulator/emulator_us.gba.lz"
